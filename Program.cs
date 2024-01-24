d65d2b6c42eb44c16a1ba910a57af127c009053b
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-01-24 12:55:31 -0800

    Updated Program File

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace GA_Strings_Ishak
+﻿using System.Xml.Linq;
+
+namespace GA_Strings_Ishak
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Clasmates!");
+            string myFirstName = "Adam"; 
+            Console.WriteLine(myFirstName);
+            string myLastName = "Ishak";
+            Console.WriteLine(myLastName);
+            string myFullName = myFirstName + " " + myLastName;
+            Console.WriteLine(myFullName);
+            // Reassigning my teachers name to myFirstName
+            myFirstName = "William";
+            // Reassigning my teachers name to myLastName
+            myLastName = "Cram";
+            // Adding another string to myFullName
+            myFullName = myFirstName + " " + myLastName + " is my professor";
+            // Displaying the result
+            Console.WriteLine(myFullName);
+
+
         }
     }
 }
